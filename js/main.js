3361b1bccf95baae25229dbd94504f08a8d59676
Yura Solomko <solomkoyv@gmail.com>
2018-10-26 18:52:37 +0300

    Обязательное задание к Уроку №14 +

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+	$('[href="#sheldure"]').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btn').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
 	$('.main_btna').on('click', () => {
 		$('.overlay').fadeToggle('slow');
 		$('.modal').slideDown("slow");
